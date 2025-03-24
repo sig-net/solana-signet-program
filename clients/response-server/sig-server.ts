02416d40d95ee61417a467568d9be37ad446f1f3
Osman Abdelnasir <esaminu1@icloud.com>
2025-03-25 00:28:58 +0400

    feat: add string private key

--- clients/response-server/sig-server.ts
+++ clients/response-server/sig-server.ts
@@ -43,12 +43,25 @@ function generateRequestId(
 }
 
 function loadSolanaKeypair(): Keypair {
-  const keypairPath =
-    process.env.KEYPAIR_PATH ||
-    path.join(os.homedir(), ".config", "solana", "id.json");
-  const keypairString = fs.readFileSync(keypairPath, { encoding: "utf-8" });
-  const keypairData = JSON.parse(keypairString);
-  return Keypair.fromSecretKey(new Uint8Array(keypairData));
+  if (process.env.SOLANA_PRIVATE_KEY) {
+    const privateKeyString = process.env.SOLANA_PRIVATE_KEY;
+    try {
+      const privateKey = JSON.parse(privateKeyString);
+      return Keypair.fromSecretKey(new Uint8Array(privateKey));
+    } catch (e) {
+      throw new Error(`Failed to parse SOLANA_PRIVATE_KEY: ${e}`);
+    }
+  }
+  
+  try {
+    const keypairPath = process.env.KEYPAIR_PATH || 
+      path.join(os.homedir(), ".config", "solana", "id.json");
+    const keypairString = fs.readFileSync(keypairPath, { encoding: "utf-8" });
+    const keypairData = JSON.parse(keypairString);
+    return Keypair.fromSecretKey(new Uint8Array(keypairData));
+  } catch (e) {
+    throw new Error(`Failed to load keypair from file: ${e}`);
+  }
 }
 
 function deriveEpsilonSol(requester: string, path: string): bigint {
