f9d7c0dd06c1e953bbc41e4581a29219149b9ec5
Pessina <fs.pessina@gmail.com>
2025-06-29 18:02:56 +0500

    Include configuration tests

---
+++ signet-program/test-utils/solana.ts
@@ -0,0 +1,17 @@
+import * as anchor from "@coral-xyz/anchor";
+
+export const confirmTransaction = async (
+  connection: anchor.web3.Connection,
+  signature: string
+) => {
+  const latestBlockhash = await connection.getLatestBlockhash();
+
+  await connection.confirmTransaction(
+    {
+      signature,
+      blockhash: latestBlockhash.blockhash,
+      lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+    },
+    "confirmed"
+  );
+};
